07f9240a5a163fc4da19d000c603ed4c12315e93
Stephen Basile <stephen@fastly.com>
2016-08-31 10:04:10 -0400

    Finish up waf command

--- lib/vcl/commands/waf.rb
+++ lib/vcl/commands/waf.rb
@@ -1,36 +1,24 @@
 module VCL
   class CLI < Thor
     desc "waf", "Download WAF VCLs"
+    option :service
     def waf
-      parsed_id = VCL::Utils.parse_directory
-
-      if options[:service]
-        abort "Already in a service directory, go up one level in order to specify"\
-              "service id with --service." if parsed_id
-        id = options[:service]
-        parsed = false
+      if !options[:service1]
+        id = VCL::Utils.parse_directory
+        abort "Could not parse service id from directory" unless id
       else
-        abort "Could not parse service id from directory. Specify service id with "\
-              "--service option or use from within service directory." unless parsed_id
-        id = parsed_id
-        parsed = true
+        id = options[:service]
       end
+
+      version = options[:version] ? options[:version] : VCL::Fetcher.get_active_version(id) 
 
       service = VCL::Fetcher.api_request(:get, "/service/#{id}/details")
 
-      customer_id = service["customer_id"]
-
-      VCL::Fetcher.assume_account_owner(customer_id)
-
-      waf = VCL::Fetcher.api_request(:get, "/service/#{id}/wafs/4o0eNn2qV1zmqfosNx4CwQ/ruleset", :endpoint => :app)
-      
-      VCL::Fetcher.unassume
-
-      folder_name = parsed ? "./" : "#{service["name"]} - #{service["id"]}/"
-      Dir.mkdir(folder_name) unless (File.directory?(folder_name) || parsed)
+      waf = service["active_version"]["wafs"]
 
       if waf
-        filename = "#{folder_name}ruleset.waf"
+        waf_data = VCL::Fetcher.api_request(:get, "/service/#{id}/wafs/#{waf[0]["id"]}/ruleset")
+        filename = "ruleset.waf"
 
         if File.exist?(filename)
           unless yes?("Are you sure you want to overwrite #{filename}")
@@ -39,7 +27,7 @@ module VCL
           end
         end
 
-        File.open(filename, 'w+') {|f| f.write(waf["data"]["attributes"]["vcl"]) }
+        File.open(filename, 'w+') {|f| f.write(JSON.parse(waf_data)["data"]["attributes"]["vcl"]) }
 
         say("WAF VCL content written to #{filename}")
       else
