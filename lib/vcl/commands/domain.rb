278bd77879d81e73f6a13d572fc1cb915d4982e0
Stephen Basile <stephen@fastly.com>
2017-05-10 11:31:11 -0400

    beef up domainc command

--- lib/vcl/commands/domain.rb
+++ lib/vcl/commands/domain.rb
@@ -1,9 +1,9 @@
 module VCL
   class CLI < Thor
-    desc "domain ACTION HOST", "Create and delete domains on a service."
+    desc "domain ACTION HOST", "Manipulate domains on a service. Available actions are create, delete, list and check. Create, delete and check take a host argument. Additionally, check can take the argument \"all\" to check all domains."
     method_option :service, :aliases => ["--s"]
     method_option :version, :aliases => ["--v"]
-    def domain(action,host)
+    def domain(action,host=false)
       id = VCL::Utils.parse_directory unless options[:service]
       id ||= options[:service]
 
@@ -23,6 +23,21 @@ module VCL
       when "delete"
         VCL::Fetcher.api_request(:delete,"/service/#{id}/version/#{version}/domain/#{host}")
         puts "#{host} deleted on #{id} version #{version}"
+      when "list"
+        domains = VCL::Fetcher.api_request(:get,"/service/#{id}/version/#{version}/domain")
+        domains.each do |d|
+          puts d["name"]
+        end
+      when "check"
+        if host == "all"
+          domains = VCL::Fetcher.api_request(:get,"/service/#{id}/version/#{version}/domain/check_all")
+        else
+          domains = [VCL::Fetcher.api_request(:get,"/service/#{id}/version/#{version}/domain/#{host}/check")]
+        end
+
+        domains.each do |d|
+          puts "#{d[0]["name"]} -> #{d[1]}"
+        end
       else
         abort "#{action} is not a valid command"
       end
