1cea05bdfab3d39c5b0a2a6a12ca45b104906e2c
Stephen Basile <stephen@fastly.com>
2018-12-20 16:06:06 -0500

    dont allow header splitting with the key

--- lib/vcl.rb
+++ lib/vcl.rb
@@ -22,5 +22,5 @@ module VCL
   TANGO_PATH = "/configure/services/"
 
   Cookies = File.exist?(VCL::COOKIE_JAR) ? JSON.parse(File.read(VCL::COOKIE_JAR)) : {}
-  Token = File.exist?(VCL::TOKEN_FILE) ? File.read(VCL::TOKEN_FILE) : false
-end
+  # Don't allow header splitting with the key
+  Token = File.exist?(VCL::TOKEN_FILE) ? File.read(VCL::TOKEN_FILE).gsub(/\r|\n/,'') : false
